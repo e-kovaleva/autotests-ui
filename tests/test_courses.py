ca2099777dcd420747105b25de3137ee37ee1adc
Ekaterina Kovaleva <fullmoon010191@gmail.com>
2025-08-10 15:58:21 +0300

    7.3 Практика работы с Page Component

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -6,26 +6,15 @@ from pages.create_course_page import CreateCoursePage
 
 @pytest.mark.regression
 @pytest.mark.courses
-def test_empty_courses_list(chromium_page_with_state: Page):
+def test_empty_courses_list(courses_list_page: CoursesListPage):
+    courses_list_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses')
+    
+    courses_list_page.navbar.check_visible('username')
+    courses_list_page.sidebar.check_visible()
 
-    chromium_page_with_state.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses')
-
-    courses_title = chromium_page_with_state.get_by_test_id('courses-list-toolbar-title-text')
-    expect(courses_title).to_be_visible()
-    expect(courses_title).to_have_text('Courses')
-
-    icon = chromium_page_with_state.get_by_test_id('courses-list-empty-view-icon')
-    expect(icon).to_be_visible()
-
-    no_result_text = chromium_page_with_state.get_by_test_id('courses-list-empty-view-title-text')
-    expect(no_result_text).to_be_visible()
-    expect(no_result_text).to_have_text('There is no results')
-
-    no_description_text = chromium_page_with_state.get_by_test_id('courses-list-empty-view-description-text')
-    expect(no_description_text).to_be_visible()
-    expect(no_description_text).to_have_text('Results from the load test pipeline will be displayed here')
-
-    chromium_page_with_state.wait_for_timeout(5000)
+    courses_list_page.check_visible_courses_title()
+    courses_list_page.check_visible_create_course_button()
+    courses_list_page.check_visible_empty_view()
 
 
 @pytest.mark.regression
@@ -66,3 +55,23 @@ def test_create_course(courses_list_page: CoursesListPage, create_course_page: C
         max_score='100',
         min_score='10'
     )
+
+
+    # chromium_page_with_state.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses')
+
+    # courses_title = chromium_page_with_state.get_by_test_id('courses-list-toolbar-title-text')
+    # expect(courses_title).to_be_visible()
+    # expect(courses_title).to_have_text('Courses')
+
+    # icon = chromium_page_with_state.get_by_test_id('courses-list-empty-view-icon')
+    # expect(icon).to_be_visible()
+
+    # no_result_text = chromium_page_with_state.get_by_test_id('courses-list-empty-view-title-text')
+    # expect(no_result_text).to_be_visible()
+    # expect(no_result_text).to_have_text('There is no results')
+
+    # no_description_text = chromium_page_with_state.get_by_test_id('courses-list-empty-view-description-text')
+    # expect(no_description_text).to_be_visible()
+    # expect(no_description_text).to_have_text('Results from the load test pipeline will be displayed here')
+
+    # chromium_page_with_state.wait_for_timeout(5000)
