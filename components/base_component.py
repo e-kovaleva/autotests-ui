426b80f251cbe61f2435618363ca981a4450670f
Ekaterina Kovaleva <fullmoon010191@gmail.com>
2025-08-18 19:20:27 +0300

    9.5 Практика работы с логированием

--- components/base_component.py
+++ components/base_component.py
@@ -1,5 +1,9 @@
 from playwright.sync_api import expect, Page
 from typing import Pattern
+from tools.logger import get_logger
+
+
+logger = get_logger('BASE_COMPONENT')
 
 
 class BaseComponent:
@@ -7,4 +11,7 @@ class BaseComponent:
         self.page = page
 
     def check_current_url(self, expected_url: Pattern[str]):
+        step = f'Checking current url {expected_url}'
+
+        logger.info(step)
         expect(self.page).to_have_url(expected_url)
